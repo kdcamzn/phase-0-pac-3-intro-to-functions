8248e0e934dac592c15f1fff5e1ea57f53049ec3
Kelly Callaway <kdcamzn@gmail.com>
2022-03-17 03:59:36 -0600

    Played with functions, added code to index.js file

--- index.js
+++ index.js
@@ -1,1 +1,80 @@
 // Follow along with the examples here
+//function sayHello() {
+//console.log("Hello");
+//}
+//sayHello();
+
+/*function sayHelloToIsabel() {
+    console.log("Hello, Isabel!");
+}
+sayHelloToIsabel();
+
+function sayHelloToSofia() {
+    console.log("Hello, Sofia!");
+}
+sayHelloToSofia();
+
+function sayHelloToBrendan() {
+    console.log("Hello, Brendan!");
+}
+sayHelloToBrendan();
+
+
+sayHelloToIsabel();
+sayHelloToSofia();
+sayHelloToBrendan();*/
+
+/*
+function doSomething(thing) {
+    console.log(thing);
+}
+doSomething("anything");
+*/
+
+/*function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+}
+sayHelloTo("Isabel");
+sayHelloTo("Jane");
+sayHelloTo("R2-D2");
+sayHelloTo(1);
+
+console.log(firstName);
+say("Goodbye", "Julio");
+say("Julio", "hello");
+*/
+
+/*function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+    console.log("firstName:", firstName);
+    console.log("greeting: ", greeting);
+    console.log(`${greeting}, ${firstName}!`);
+}
+
+say("Julio", "hello");
+function add(x, y) {
+    return x + y;
+}
+console.log(add(1, 2));
+*/
+
+/*function say(greeting, firstName) {
+   return `${greeting}, ${firstName}!`;
+}
+console.log(say("Hello", "Sofia"));
+*/
+
+// function add(x,y) {
+//     return x + y;
+// }
+// console.log(add(1, 2));
+
+// const message = `The sum of your numbers is: ${add}.`;
+
+
+function freePets(pet1, pet2){
+    const phrase = `${pet1} and ${pet2} living together in Peace!`;
+    return phrase;
+}
+console.log(freePets(1, 2));
+
